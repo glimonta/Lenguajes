d6e254d2f493e22bd769fbd96ffdef8ba3fa49a5
Gabriela Limonta <glimonta@gmail.com>
2014-01-30 10:59:09 -0430

    Todo el esqueleto esta listo no se ha probado aun
    D:
    listo el enviar y el procesar

--- Ruby/main.rb
+++ Ruby/main.rb
@@ -19,12 +19,14 @@ end
 class Maquina
   attr_accessor :cantidadMaxima, :estado, :desecho, :ciclosProcesamiento, :cicloActual
 
-  def initialize(cantidadMax, estado, desecho, ciclosProcesamiento)
+  def initialize(cantidadMax, estado, desecho, ciclosProcesamiento,siguiente)
     @cantidadMaxima = cantidadMax
     @estado = estado
     @desecho = desecho
     @ciclosProcesamiento = ciclosProcesamiento
+    @siguiente = siguiente
     @cicloActual = 0
+    @cantidadProducida
   end
 
   def inactiva?
@@ -43,10 +45,24 @@ class Maquina
     @estado == 'llena'
   end
 
-  def enviar
+  def tengoInsumos?
+    true
   end
 
-  def procesar
+  def enviar(siguiente)
+    if !siguiente.nil? then
+      if @siguiente.cantidadPAMax <= @cantidadProducida then
+        @siguiente.cantidadPAActual = @siguiente.cantidadPAMax
+        @cantidadProducida = @cantidadProducida - @siguiente.cantidadPAMax
+      else
+        @siguiente.cantidadPAActual = @cantidadProducida
+        @cantidadProducida = 0
+        @estado = 'inactiva'
+      end
+    else
+      puts (@cantidadProducida / 4).to_s
+      @estado = 'inactiva'
+    end
   end
 
   def to_s
@@ -62,27 +78,37 @@ end
 
 class Maquina2 < Maquina
   attr_accessor :cantidadPAActual, :cantidadPAMax
-    def initialize(cantidadMax, estado, desecho, ciclosProcesamiento, cantidadPA)
-      super(cantidadMax, estado, desecho, ciclosProcesamiento)
-      @cantidadPAActual = 0
-      @cantidadPAMax    = cantidadPA
+
+  def initialize(cantidadMax, estado, desecho, ciclosProcesamiento, siguiente, cantidadPA)
+    super(cantidadMax, estado, desecho, ciclosProcesamiento, siguiente)
+    @cantidadPAActual = 0
+    @cantidadPAMax    = cantidadPA
+  end
+
+  def to_s
+    str = ''
+    if self.inactiva? || self.llena? then
+      str = "Cantidad de Producto de Maquina Anterior: #{@cantidadPAActual.to_s}\n"
     end
 
-    def to_s
-      str = ''
-      if self.inactiva? || self.llena? then
-        str = "Cantidad de Producto de Maquina Anterior: #{@cantidadPAActual.to_s}\n"
-      end
+    super + str
+  end
 
-      super + str
-    end
+  def tengoInsumos?
+    super && @cantidadPAActual == @cantidadPAMax
+  end
+
+  def eliminarInsumos
+    @cantidadPAActual = 0
+  end
+
 end
 
-def generaMaquina(superclase, nombre, mixins)
+def generaMaquina(superclase, nombre, siguiente, mixins)
   clase = Class::new(superclase) do
    include mixins.first unless mixins.first.nil?
 
-    def initialize(cantidadMaxima, estado, desecho, ciclosProcesamiento, cantidadInsumo=nil, cantidadPA=nil)
+    def initialize(cantidadMaxima, estado, desecho, ciclosProcesamiento, siguiente, cantidadInsumo=nil, cantidadPA=nil)
       if self.class.included_modules.include?(RecibeCebada) then
           @cantidadCMax    = cantidadInsumo
           @cantidadCActual = 0
@@ -98,9 +124,9 @@ def generaMaquina(superclase, nombre, mixins)
       end
 
      if cantidadPA.nil? then
-       super(cantidadMaxima, estado, desecho, ciclosProcesamiento)
+       super(cantidadMaxima, estado, desecho, ciclosProcesamiento, siguiente)
      else
-       super(cantidadMaxima, estado, desecho, ciclosProcesamiento, cantidadPA)
+       super(cantidadMaxima, estado, desecho, ciclosProcesamiento, siguiente, cantidadPA)
      end
     end
 
@@ -117,23 +143,63 @@ def generaMaquina(superclase, nombre, mixins)
       super + str + "\n"
     end
 
+    def tengoInsumos?
+      insumos = super
+      insumos = insumos && @cantidadCActual == @cantidadCMax if self.class.included_modules.include?(RecibeCebada)
+      insumos = insumos && @cantidadMActual == @cantidadMMax if self.class.included_modules.include?(RecibeMezcla)
+      insumos = insumos && @cantidadLActual == @cantidadLMax if self.class.included_modules.include?(RecibeLupulo)
+      insumos = insumos && @cantidadVActual == @cantidadVMax if self.class.included_modules.include?(RecibeLevadura)
+      insumos
+    end
+
+    def eliminarInsumos
+      super
+      @cantidadCActual = 0 if self.class.included_modules.include?(RecibeCebada)
+      @cantidadMActual = 0 if self.class.included_modules.include?(RecibeMezcla)
+      @cantidadLActual = 0 if self.class.included_modules.include?(RecibeLupulo)
+      @cantidadVActual = 0 if self.class.included_modules.include?(RecibeLevadura)
+    end
+
+    def procesar
+      if inactiva? then
+        if tengoInsumos? then
+          @estado = 'llena'
+        end
+      elsif llena? then
+        @estado = 'procesando'
+      elsif procesando? then
+        if @cicloActual < @ciclosProcesamiento then
+          @cicloActual = @cicloActual.succ
+        else
+          @cicloActual = 0
+          @cantidadProducida = @capacidadMaxima * (1 - @desecho)
+          eliminarInsumos
+          @estado = 'en espera'
+        end
+      elsif en_espera? then
+        if @siguiente.inactiva? then
+          enviar(@siguiente)
+        end
+      end
+    end
+
   end
 
   Object::const_set nombre, clase
 end
 
 
-generaMaquina(Maquina, 'Silos_de_Cebada', [RecibeCebada])
-  generaMaquina(Maquina2, 'Molino'                        , [])
-  generaMaquina(Maquina2, 'Paila_de_Mezcla'               , [RecibeMezcla])
-  generaMaquina(Maquina2, 'Cuba_de_Filtracion'            , [])
-  generaMaquina(Maquina2, 'Paila_de_Coccion'              , [RecibeLupulo])
-  generaMaquina(Maquina2, 'Tanque_preclarificador'        , [])
-  generaMaquina(Maquina2, 'Enfriador'                     , [])
-  generaMaquina(Maquina2, 'TCC'                           , [RecibeLevadura])
-  generaMaquina(Maquina2, 'Filtro_de_Cerveza'             , [])
-  generaMaquina(Maquina2, 'Tanques_para_Cerveza_Filtrada' , [])
-  generaMaquina(Maquina2, 'Llenadora_y_Tapadora'          , [])
+generaMaquina(Maquina, 'Silos_de_Cebada', 'Molino', [RecibeCebada])
+  generaMaquina(Maquina2, 'Molino'                        , 'Paila_de_Mezcla'               ,[])
+  generaMaquina(Maquina2, 'Paila_de_Mezcla'               , 'Cuba_de_Filtracion'            ,[RecibeMezcla])
+  generaMaquina(Maquina2, 'Cuba_de_Filtracion'            , 'Paila_de_Coccion'              ,[])
+  generaMaquina(Maquina2, 'Paila_de_Coccion'              , 'Tanque_preclarificador'        ,[RecibeLupulo])
+  generaMaquina(Maquina2, 'Tanque_preclarificador'        , 'Enfriador'                     ,[])
+  generaMaquina(Maquina2, 'Enfriador'                     , 'TCC'                           ,[])
+  generaMaquina(Maquina2, 'TCC'                           , 'Filtro_de_Cerveza'             ,[RecibeLevadura])
+  generaMaquina(Maquina2, 'Filtro_de_Cerveza'             , 'Tanques_para_Cerveza_Filtrada' ,[])
+  generaMaquina(Maquina2, 'Tanques_para_Cerveza_Filtrada' , 'Llenadora_y_Tapadora'          ,[])
+  generaMaquina(Maquina2, 'Llenadora_y_Tapadora'          ,  nil                            ,[])
 
 
 def main
