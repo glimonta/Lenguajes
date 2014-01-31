9f8bc3cda6aa47b6f3f3832965b686d78e930451
Gabriela Limonta <glimonta@gmail.com>
2014-01-31 13:00:10 -0430

    Separe el main en 3
    En main se encuentra solamente el main que se ejecutará.
    En Maquina la declaracion de la clase Maquina y sus metodos.
    En Maquina2 la declaracion de la clase Maquina2 y la generacion de las demas subclases.

---
+++ Ruby/Maquina2.rb
@@ -0,0 +1,72 @@
+class Maquina2 < Maquina
+  attr_accessor :cantidadPAActual, :cantidadPAMax
+
+  def initialize(cantidadMax, desecho, ciclosProcesamiento, siguiente, cantidadPA)
+    super(cantidadMax, desecho, ciclosProcesamiento, siguiente)
+    @cantidadPAActual = 0
+    @cantidadPAMax    = cantidadPA
+  end
+
+  def to_s
+    str = ''
+    if self.inactiva? || self.llena? then
+      str = "Cantidad de Producto de Maquina Anterior: #{@cantidadPAActual.to_s}\n"
+    end
+
+    super + str
+  end
+
+  def puedoTomarInsumos?
+    super && @cantidadPAActual == cantidadPAMax
+  end
+
+  def eliminarInsumos
+    super
+    @cantidadPAActual = 0
+  end
+
+end
+
+def generaMaquina(superclase, nombre, siguiente, mixins)
+  clase = Class::new(superclase) do
+   include mixins.first unless mixins.first.nil?
+
+    def initialize(cantidadMaxima, desecho, ciclosProcesamiento, siguiente, cantidadInsumo=nil, cantidadPA=nil)
+      if self.class.included_modules.include?(RecibeCebada) then
+          @cantidadCMax    = cantidadInsumo
+          @cantidadCActual = 0
+      elsif self.class.included_modules.include?(RecibeMezcla) then
+          @cantidadMMax    = cantidadInsumo
+          @cantidadMActual = 0
+      elsif self.class.included_modules.include?(RecibeLupulo) then
+          @cantidadLMax    = cantidadInsumo
+          @cantidadLActual = 0
+      elsif self.class.included_modules.include?(RecibeLevadura) then
+          @cantidadVMax    = cantidadInsumo
+          @cantidadVActual = 0
+      end
+
+     if cantidadPA.nil? then
+       super(cantidadMaxima, desecho, ciclosProcesamiento, siguiente)
+     else
+       super(cantidadMaxima, desecho, ciclosProcesamiento, siguiente, cantidadPA)
+     end
+    end
+
+  end
+
+  Object::const_set nombre, clase
+end
+
+
+generaMaquina(Maquina, 'Silos_de_Cebada', nil, [RecibeCebada])
+  generaMaquina(Maquina2, 'Molino'                        , nil ,[])
+  generaMaquina(Maquina2, 'Paila_de_Mezcla'               , nil ,[RecibeMezcla])
+  generaMaquina(Maquina2, 'Cuba_de_Filtracion'            , nil ,[])
+  generaMaquina(Maquina2, 'Paila_de_Coccion'              , nil ,[RecibeLupulo])
+  generaMaquina(Maquina2, 'Tanque_Preclarificador'        , nil ,[])
+  generaMaquina(Maquina2, 'Enfriador'                     , nil ,[])
+  generaMaquina(Maquina2, 'TCC'                           , nil ,[RecibeLevadura])
+  generaMaquina(Maquina2, 'Filtro_de_Cerveza'             , nil ,[])
+  generaMaquina(Maquina2, 'Tanques_para_Cerveza_Filtrada' , nil ,[])
+  generaMaquina(Maquina2, 'Llenadora_y_Tapadora'          , nil ,[])
